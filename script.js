f233872739e1cb568ff3dee853f814f161ef7ada
rezadika11 <rezadika11@gmail.com>
2023-01-05 13:22:22 +0700

    main

---
+++ script.js
@@ -0,0 +1,19 @@
+const nilai = 62;
+
+if (nilai >= 81) {
+    console.log("anda mendapatkan score A");
+} else if (nilai >= 61){
+    console.log("anda mendapatkan score B");
+}else if (nilai >= 41){
+    console.log("anda mendapatkan score C");
+}else if (nilai >= 20){
+    console.log("anda mendapatkan score D");
+} else {
+    console.log("anda mendapatkan score E");
+}
+
+// 81 - 100 "anda mendapatkan score A";
+// 61 - 80 "anda mendapatkan score B";
+// 41 - 60 "anda mendapatkan score C";
+// 20 - 40 "anda mendapatkan score D";
+// < 20 "anda mendapatkan score E";
